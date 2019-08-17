0e5a2a4ca5b0b989a351e6971019913ac59ff2f5
Alexander <ahcarlin@gmail.com>
2019-08-16 19:05:21 -0500

    MapScreen cleanup, node_modules pared down

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import * as Permissions from 'expo-permissions';
 import {PermissionsAndroid} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-// import Geolocation from 'react-native-geolocation-service';
 
 export default function MapScreen() {
     const [position, setPosition] = useState({coords:{latitude:0, longitude:0}});
@@ -23,21 +22,12 @@ export default function MapScreen() {
 
     useEffect(() => {
         requestGeolocationPermission();
-
-        // Geolocation.getCurrentPosition(
-        //     (pos) => {
-        //         console.log(pos)
-        //         return myPosition = pos;
-        //     },
-        //     (err) => {console.log(err.code, err.message)},
-        //     {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000})
     }, [])
     
 
-    // let currentPosition = geolocation
+
     return (
         <MapView
-        re
             style={{flex: 1}}
             provider={PROVIDER_GOOGLE}
             showsUserLocation={true}
@@ -47,12 +37,6 @@ export default function MapScreen() {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421
             }}
-            // initialRegion={{
-            //     latitude: 37.78825,
-            //     longitude: -122.4324,
-            //     latitudeDelta: 0.0922,
-            //     longitudeDelta: 0.0421
-            // }}
         />
     )
 }
