b7eeb46cca9ca28e3cc3a08db5a6a3c90f0197da
Alexander <ahcarlin@gmail.com>
2019-08-16 20:20:18 -0500

    Deltas constantized, StyleSheet added on MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,21 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import * as Permissions from 'expo-permissions';
-import {PermissionsAndroid} from 'react-native';
+import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function MapScreen() {
+    const LAT_DELTA = 0.0922;
+    const LONG_DELTA = 0.0421;
+
     const [position, setPosition] = useState({coords:{latitude:0, longitude:0}});
 
     async function requestGeolocationPermission() {
         const {status, permissions} = await Permissions.askAsync(Permissions.LOCATION);
         if (status === 'granted') {
             return navigator.geolocation.getCurrentPosition(
-                (pos) => {
-                    setPosition(pos);
-                    console.log(position)
-                        },
-                        (err) => {console.log(err.code, err.message)},
-                        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+                (pos) => setPosition(pos),
+                (err) => console.log(err.code, err.message),
+                {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
             )
         }
     }
@@ -24,19 +24,24 @@ export default function MapScreen() {
         requestGeolocationPermission();
     }, [])
     
-
-
     return (
         <MapView
-            style={{flex: 1}}
+            style={styles.container}
             provider={PROVIDER_GOOGLE}
             showsUserLocation={true}
             region={{
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
+                latitudeDelta: LAT_DELTA,
+                longitudeDelta: LONG_DELTA
             }}
         />
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        maxHeight: "45%"
+    }
+})
